be7053eac0dda8b56c573db9851e87e2bec3dd4c
Jimmy Dee <jgvdthree@gmail.com>
2019-08-22 15:56:39 -0700

    Also avoid gtest, gmock in stage

--- BranchSDK/tools/stage.py
+++ BranchSDK/tools/stage.py
@@ -40,7 +40,7 @@ makedirs("stage/lib")
 
 # Skip Poco's MongoDB and SQLiteData modules, which we don't require.
 # Also CppUnit, which one of our deps uses.
-excludes = ["CppUnit", "*MongoDB*", "*DataSQLite*"]
+excludes = ["CppUnit", "*gtest*", "*gmock*", "*MongoDB*", "*DataSQLite*"]
 
 for item in installed:
     recipe = item["recipe"]
