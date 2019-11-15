07f87615a381d64765c2eca8c068aaa9450e8d7a
Andy Peterson <apeterson@branch.io>
2019-11-15 11:36:46 -0800

    Implemented a registry install menu option

--- BranchSDK-Samples/Windows/ColorPicker/resource.h
+++ BranchSDK-Samples/Windows/ColorPicker/resource.h
@@ -13,6 +13,9 @@
 #define IDC_COLORPICKER                 109
 #define IDR_MAINFRAME                   128
 #define ID_FILE_PICKACOLOR              32771
+#define ID_FILE_INSTALL                 32772
+#define IDM_PICKACOLOR                  32774
+#define IDM_INSTALL                     32775
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -21,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
