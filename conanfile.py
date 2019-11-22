64d1e6fe47404442ad73427fec40cbfdfb59bf71
Andy Peterson <apeterson@branch.io>
2019-11-22 13:21:35 -0800

    Set version to 1.0.0

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class BranchioConan(ConanFile):
     # ----- Package metadata -----
     name = "BranchIO"
     # TODO(jdee): Set the version in one place and propagate it
-    version = "0.2.1"
+    version = "1.0.0"
     license = "MIT"
     description = "Branch Metrics deep linking and attribution analytics C++ SDK"
     topics = (
