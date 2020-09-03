6ef6d4837a189af0353920d6e556a2633fc66e52
Jimmy Dee <jimmy.dee@branch.io>
2020-09-03 13:19:02 -0700

    whitespace

--- BranchSDK-Samples/Windows/TestBed/TestBed.cpp
+++ BranchSDK-Samples/Windows/TestBed/TestBed.cpp
@@ -49,6 +49,7 @@ initBranch(const std::wstring& key)
 
     return Branch::create(key, &appInfo);
 }
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
