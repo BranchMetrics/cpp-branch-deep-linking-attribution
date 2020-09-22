ec5e6010ee70b0e03bb958ef1da11a4e5709e865
Jimmy Dee <jimmy.dee@branch.io>
2020-09-22 14:08:24 -0700

    whitespace

--- BranchSDK/src/BranchIO/Util/RequestManager.cpp
+++ BranchSDK/src/BranchIO/Util/RequestManager.cpp
@@ -161,8 +161,8 @@ RequestManager::RequestTask::runTask() {
         payload.remove(Defines::JSONKEY_SESSION_FINGERPRINT);      // device_fingerprint_id
         payload.remove(Defines::JSONKEY_SESSION_IDENTITY);         // identity_id
 
-	// Clear out storage as well
-	Storage::instance().remove("session.device_fingerprint_id");
+	    // Clear out storage as well
+	    Storage::instance().remove("session.device_fingerprint_id");
     }
 
     // Send request synchronously
