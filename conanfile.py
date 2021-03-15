8fa916783088b8f33249e0d475d5e16907712a29
Jimmy Dee <jimmy.dee@branch.io>
2021-03-15 11:23:19 -0700

    Version bump to 1.2.1

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class BranchioConan(ConanFile):
     # ----- Package metadata -----
     name = "BranchIO"
     # TODO(jdee): Set the version in one place and propagate it
-    version = "1.2.1-alpha.3"
+    version = "1.2.1"
     license = "MIT"
     description = "Branch Metrics deep linking and attribution analytics C++ SDK"
     topics = (
