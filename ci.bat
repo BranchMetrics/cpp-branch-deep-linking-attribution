af6be71386bd8ed5009d900360edde5c14636234
Jimmy Dee <jimmy.dee@branch.io>
2021-04-27 13:58:35 -0700

    Both 64- and 32-bit builds

--- ci.bat
+++ ci.bat
@@ -1,11 +1,14 @@
 echo off
 
+REM .\ci.bat 64
+REM .\ci.bat 32
+
 REM Look for VS 2019
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\" (
-  call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+  call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars%1.bat"
 ) else (
   if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\" (
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars%1.bat"
   ) else (
     echo Could not find Visual Studio 2019
     exit /b 1
