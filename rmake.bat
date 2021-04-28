f0b702f708719d86033b6ad1f6b6a54718137fc3
Jimmy Dee <jimmy.dee@branch.io>
2021-04-28 11:15:39 -0700

    Revert "Try using --update with lockfiles"
    This reverts commit af1decdacf087efe4da4bfdcafc5e41c2c3dd0e2.

--- rmake.bat
+++ rmake.bat
@@ -111,7 +111,6 @@ if %BUILD_TYPE% == Debug (
 REM Build/install dependencies needed for this SDK
 conan install ..\..\conanfile.py^
   --build outdated^
-  --update^
   --lockfile=..\..\locks\%BUILD_TYPE%-%TARGET_ARCH%.lock
 
 if ERRORLEVEL 1 (
