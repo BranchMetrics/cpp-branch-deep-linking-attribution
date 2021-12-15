ac8f3a7f33200489f1fdac9e82602c135642b843
echo-branch <echo@branch.io>
2021-12-15 10:13:58 -0800

    Working on reworkign the cmake files

--- BranchSDK/cmake/Windows.cmake
+++
@@ -1,8 +0,0 @@
-# On Windows, link against BranchIO.lib for the DLL
-# or BranchIOStatic.lib for the static library.
-# Must define BRANCHIO_DLL when including headers and building against the DLL (as opposed to the static lib)
-
-# Define when building the DLL on Windows
-if (BUILD_SHARED_LIBS)
-    set_target_properties(BranchIO PROPERTIES DEFINE_SYMBOL BRANCHIO_BUILD_DLL)
-endif()
