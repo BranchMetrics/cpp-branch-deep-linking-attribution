b77c5f531cb9c9bd7cd9953325cbfcc38f81483e
NidhiDixit09 <93544270+NidhiDixit09@users.noreply.github.com>
2023-02-16 00:00:41 -0800

    Updated version to 2.0.1

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class BranchioConan(ConanFile):
 
     # ----- Package metadata -----
     name = "BranchIO"
-    version = "1.2.3"
+    version = "2.0.1"
     license = "MIT"
     description = "Branch Metrics deep linking and attribution analytics C++ SDK"
     topics = (
